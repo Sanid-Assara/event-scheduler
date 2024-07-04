3dc23dedba330ea110bff384c799f210d6d2d8ae
Sanid-Assara <sanid.assara@gmail.com>
2024-07-05 00:15:11 +0200

    Route and Style NotFoundPage.

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,9 +1,14 @@
 import { NavLink } from "react-router-dom";
 export default function PageNotFound() {
   return (
-    <>
+    <div className="flex flex-col justify-center items-center gap-8 h-svh text-2xl font-bold">
       <p>404 Page not found</p>
-      <NavLink to="/"> Go To Home Page</NavLink>
-    </>
+      <NavLink
+        to="/"
+        className={"outline px-4 py-2 hover:bg-black hover:text-white"}
+      >
+        Go To Home Page
+      </NavLink>
+    </div>
   );
 }
